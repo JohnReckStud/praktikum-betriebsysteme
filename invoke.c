d2117ecdd7dc5a4b87d82ef44db46c355ce57c5a
JohnReckStud <john.reck@stud.hs-emden-leer.de>
2025-05-24 21:52:12 +0200

    Update invoke.c

--- invoke.c
+++ invoke.c
@@ -2,27 +2,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
-int main() {
-    // Umgebungsvariable lesen
-    const char *env = getenv("TOBEINVOKED");
+int main(int argc, char *argv[], char *envp[]) {
+    const char *prog = getenv("TOBEINVOKED");
 
-    if (env == NULL) {
-        fprintf(stderr, "Umgebungsvariable TOBEINVOKED ist nicht gesetzt.\n");
+    if (prog == NULL) {
+        fprintf(stderr, "Fehler: Umgebungsvariable TOBEINVOKED ist nicht gesetzt.\n");
         return 1;
     }
 
-    // Entscheidung: welchen Befehl ausführen
-    if (strcmp(env, "date") == 0) {
-        execlp("date", "date", (char *)NULL);
-    } else if (strcmp(env, "cal") == 0) {
-        execlp("cal", "cal", (char *)NULL);
-    } else {
-        fprintf(stderr, "Unbekannter Wert für TOBEINVOKED: %s\n", env);
+    if (strcmp(prog, "date") != 0 && strcmp(prog, "cal") != 0) {
+        fprintf(stderr, "Fehler: TOBEINVOKED muss entweder \"date\" oder \"cal\" sein.\n");
         return 1;
     }
 
-    // Falls execlp fehlschlägt
-    perror("execlp");
-    return 1;
+    if (argc != 2) {
+        fprintf(stderr, "Benutzung: %s <Anzahl>\n", argv[0]);
+        return 1;
+    }
+
+    int anzahl = atoi(argv[1]);
+    if (anzahl <= 0) {
+        fprintf(stderr, "Ungültige Anzahl: %s\n", argv[1]);
+        return 1;
+    }
+
+    for (int i = 0; i < anzahl; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Kindprozess
+            execlp(prog, prog, (char *)NULL);
+            perror("execlp"); // wird nur erreicht, wenn execlp fehlschlägt
+            exit(1);
+        } else if (pid > 0) {
+            wait(NULL); // Elternprozess wartet
+        } else {
+            perror("fork");
+            return 1;
+        }
+    }
+
+    return 0;
 }
